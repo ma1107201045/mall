6233d0b17bdf2e7de32a79a37f6adaa2e6c1ae98
maweiyan <1107201045@qq.com>
2023-04-28 14:28:15 +0800

    init project

---
+++ mall-gateway/src/main/java/com/lingyi/mall/gateway/MallGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.lingyi.mall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author maweiyan
+ * @email 1107201045@qq.com
+ * @datetime 2023/4/28 10:35
+ * @description 网关服务
+ */
+@SpringBootApplication
+public class MallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallGatewayApplication.class, args);
+    }
+}
