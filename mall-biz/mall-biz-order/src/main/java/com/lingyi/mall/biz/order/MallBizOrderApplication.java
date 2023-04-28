788259c856a994edabbaf1fa7e55df251c4de919
maweiyan <1107201045@qq.com>
2023-04-28 15:23:45 +0800

    init project of order module

---
+++ mall-biz/mall-biz-order/src/main/java/com/lingyi/mall/biz/order/MallBizOrderApplication.java
@@ -0,0 +1,13 @@
+package com.lingyi.mall.biz.order;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author maweiyan
+ * @email 1107201045@qq.com
+ * @datetime 2023/4/28 15:05
+ * @description 订单服务
+ */
+@SpringBootApplication
+public class MallBizOrderApplication {
+}
