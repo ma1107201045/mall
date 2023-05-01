58c0e3ca10f87ec5d40fbde43a37a38171ac9437
maweiyan <1107201045@qq.com>
2023-05-02 00:16:06 +0800

    add E:\IdeaProjects\mall\mall-auth\mall-auth-background\src\test\java\com\lingyi\mall\auth\background\MallAuthBackgroundApplicationTests.java

---
+++ mall-auth/mall-auth-background/src/test/java/com/lingyi/mall/auth/background/MallAuthBackgroundApplicationTests.java
@@ -0,0 +1,21 @@
+package com.lingyi.mall.auth.background;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author maweiyan
+ * @email 1107201045@qq.com
+ * @datetime 2023/5/1 17:10
+ * @description
+ */
+@SpringBootTest
+public class MallAuthBackgroundApplicationTests {
+
+    @Test
+    void test01() {
+
+    }
+}
