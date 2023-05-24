4001349ce114130f26b85416bf3b9411a1683186
maweiyan <1107201045@qq.com>
2023-05-24 15:29:24 +0800

    会员服务表设计以及实体类2

--- db/mall.sql
+++ db/mall.sql
@@ -11,11 +11,177 @@
  Target Server Version : 50739 (5.7.39-log)
  File Encoding         : 65001
 
- Date: 19/05/2023 17:53:46
+ Date: 24/05/2023 15:23:18
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for mbm_member
+-- ----------------------------
+DROP TABLE IF EXISTS `mbm_member`;
+CREATE TABLE `mbm_member`  (
+  `id` bigint(20) UNSIGNED NOT NULL COMMENT '主键id',
+  `member_level_id` bigint(20) UNSIGNED NOT NULL COMMENT '会员等级表',
+  `user_name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户名',
+  `password` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '密码',
+  `nickname` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '昵称',
+  `sex` tinyint(4) UNSIGNED NULL DEFAULT NULL COMMENT '性别：1 男；2 女',
+  `birthday` date NULL DEFAULT NULL COMMENT '生日',
+  `phone_number` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '手机号码',
+  `head_portrait` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '头像',
+  `personalized_signature` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '个性签名',
+  `is_enable` tinyint(4) UNSIGNED NOT NULL COMMENT '是否启用 1 是 0 否',
+  `register_source` tinyint(4) UNSIGNED NOT NULL COMMENT '注册来源 1.Web端 2.Android端 3.IOS端 4.PC端',
+  `register_data_time` datetime NOT NULL COMMENT '注册时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '会员管理服务-会员表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of mbm_member
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for mbm_member_level
+-- ----------------------------
+DROP TABLE IF EXISTS `mbm_member_level`;
+CREATE TABLE `mbm_member_level`  (
+  `id` bigint(20) NOT NULL COMMENT '主键id',
+  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '名称',
+  `growth_point` int(11) UNSIGNED NULL DEFAULT NULL COMMENT '增长点',
+  `is_default_level` tinyint(4) UNSIGNED NULL DEFAULT NULL COMMENT '是否为默认等级：0->不是；1->是',
+  `free_freight_point` decimal(10, 2) UNSIGNED NULL DEFAULT NULL COMMENT '免运费标准',
+  `comment_growth_point` int(11) UNSIGNED NULL DEFAULT NULL COMMENT '每次评价获取的成长值',
+  `priviledge_free_freight` tinyint(1) UNSIGNED NULL DEFAULT NULL COMMENT '是否有免邮特权',
+  `priviledge_sign_in` tinyint(1) UNSIGNED NULL DEFAULT NULL COMMENT '是否有签到特权',
+  `priviledge_comment` tinyint(1) UNSIGNED NULL DEFAULT NULL COMMENT '是否有评论获奖励特权',
+  `priviledge_promotion` tinyint(1) UNSIGNED NULL DEFAULT NULL COMMENT '是否有专享活动特权',
+  `priviledge_member_price` tinyint(1) UNSIGNED NULL DEFAULT NULL COMMENT '是否有会员价格特权',
+  `priviledge_birthday` tinyint(1) UNSIGNED NULL DEFAULT NULL COMMENT '是否有生日特权',
+  `remark` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '会员管理服务-会员等级表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of mbm_member_level
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for mbm_member_login_log
+-- ----------------------------
+DROP TABLE IF EXISTS `mbm_member_login_log`;
+CREATE TABLE `mbm_member_login_log`  (
+  `id` bigint(20) UNSIGNED NOT NULL COMMENT '主键id',
+  `member_id` bigint(20) NULL DEFAULT NULL COMMENT '会员id',
+  `ip` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '登录ip',
+  `city` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '登录所在城市',
+  `source` tinyint(4) UNSIGNED NULL DEFAULT NULL COMMENT '登录来源',
+  `create_data_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '会员管理服务-会员登录日志表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of mbm_member_login_log
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for mbp_attribute
+-- ----------------------------
+DROP TABLE IF EXISTS `mbp_attribute`;
+CREATE TABLE `mbp_attribute`  (
+  `id` bigint(20) NOT NULL COMMENT '主键id',
+  `business_id` bigint(20) NULL DEFAULT NULL COMMENT '商家id',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '商品管理服务-属性表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of mbp_attribute
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for mbp_attribute_value
+-- ----------------------------
+DROP TABLE IF EXISTS `mbp_attribute_value`;
+CREATE TABLE `mbp_attribute_value`  (
+  `id` bigint(20) NOT NULL COMMENT '主键id',
+  `business_id` bigint(20) NULL DEFAULT NULL COMMENT '商家id',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '商品管理服务-属性值表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of mbp_attribute_value
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for mbp_brand
+-- ----------------------------
+DROP TABLE IF EXISTS `mbp_brand`;
+CREATE TABLE `mbp_brand`  (
+  `id` bigint(20) NOT NULL COMMENT '主键id',
+  `business_id` bigint(20) NULL DEFAULT NULL COMMENT '商家id',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '商品管理服务-品牌表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of mbp_brand
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for mbp_category
+-- ----------------------------
+DROP TABLE IF EXISTS `mbp_category`;
+CREATE TABLE `mbp_category`  (
+  `id` bigint(20) NOT NULL COMMENT '主键id',
+  `business_id` bigint(20) NULL DEFAULT NULL COMMENT '商家id',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '商品管理服务-分类表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of mbp_category
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for mbp_category_attribute
+-- ----------------------------
+DROP TABLE IF EXISTS `mbp_category_attribute`;
+CREATE TABLE `mbp_category_attribute`  (
+  `id` bigint(20) NOT NULL COMMENT '主键id',
+  `business_id` bigint(20) NULL DEFAULT NULL COMMENT '商家id',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '商品管理服务-分类属性中间表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of mbp_category_attribute
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for mbp_sku
+-- ----------------------------
+DROP TABLE IF EXISTS `mbp_sku`;
+CREATE TABLE `mbp_sku`  (
+  `id` bigint(20) NOT NULL COMMENT '主键id',
+  `business_id` bigint(20) NULL DEFAULT NULL COMMENT '商家id',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '商品管理服务-SKU表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of mbp_sku
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for mbp_spu
+-- ----------------------------
+DROP TABLE IF EXISTS `mbp_spu`;
+CREATE TABLE `mbp_spu`  (
+  `id` bigint(20) NOT NULL COMMENT '主键id',
+  `business_id` bigint(20) NULL DEFAULT NULL COMMENT '商家id',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '商品管理服务-SPU表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of mbp_spu
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for mbs_menu
@@ -40,7 +206,7 @@ CREATE TABLE `mbs_menu`  (
   `last_modify_date_time` datetime NOT NULL COMMENT '最后修改时间',
   `is_delete` tinyint(4) UNSIGNED NOT NULL COMMENT '是否删除 1 是 0 否',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '系统管理-菜单表' ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '系统管理服务-菜单表' ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of mbs_menu
@@ -67,11 +233,12 @@ CREATE TABLE `mbs_role`  (
   `last_modify_date_time` datetime NOT NULL COMMENT '最后修改时间',
   `is_delete` tinyint(4) UNSIGNED NOT NULL COMMENT '是否删除 1是 0 否',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '系统管理-角色表' ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '系统管理服务-角色表' ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of mbs_role
 -- ----------------------------
+INSERT INTO `mbs_role` VALUES (1, '', 0, 0, NULL, '', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', 0);
 
 -- ----------------------------
 -- Table structure for mbs_role_menu
@@ -90,7 +257,7 @@ CREATE TABLE `mbs_role_menu`  (
   INDEX `fk_role_id2`(`role_id`) USING BTREE COMMENT '外键角色id索引',
   CONSTRAINT `fk_menu_id` FOREIGN KEY (`menu_id`) REFERENCES `mbs_menu` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_role_id2` FOREIGN KEY (`role_id`) REFERENCES `mbs_role` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '系统管理-角色菜单中间表' ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '系统管理服务-角色菜单中间表' ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of mbs_role_menu
@@ -121,12 +288,17 @@ CREATE TABLE `mbs_user`  (
   `is_delete` tinyint(4) UNSIGNED NOT NULL COMMENT '是否删除 1 是 0 否',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `idx_user_name`(`user_name`) USING BTREE COMMENT '用户名称唯一索引'
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '系统管理-用户表' ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '系统管理服务-用户表' ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of mbs_user
 -- ----------------------------
-INSERT INTO `mbs_user` VALUES (2434570888750079, 'admin', NULL, NULL, NULL, '$2a$10$A.FiBQNVq04i4FSaBYJL8edee7hHr6uv8eDz8dd.i25NicpgZkUu2', '123232323', NULL, NULL, NULL, NULL, 1, NULL, 'admin', '2023-05-07 16:13:03', 'admin', '2023-05-19 17:03:27', 0);
+INSERT INTO `mbs_user` VALUES (2434570888750079, 'admin', '', '', NULL, '$2a$10$JrI0QTwZbG5Ijah8fHzVtOStNqpeX88sNX.3T0cKgymqhI7SMk93C', '123', NULL, NULL, NULL, NULL, 1, NULL, 'admin', '2023-05-07 16:13:03', 'admin', '2023-05-20 04:16:55', 0);
+INSERT INTO `mbs_user` VALUES (6943878900950016, 'zs', '', '', NULL, '$2a$10$z/e9COEGndIxnhkwUmhiSOdMr8qvAva9nX95nszHodQIawRMCEsjW', NULL, NULL, NULL, NULL, NULL, 1, NULL, 'admin', '2023-05-20 03:52:30', 'admin', '2023-05-20 03:52:30', 0);
+INSERT INTO `mbs_user` VALUES (6946154164069376, 'z1s1', '', '', NULL, '$2a$10$OgRyMMcw8UYpgYJhOMg9cey212AbWUgXsVJRN.TaC9p3OPpsJlHJi', '', '', '', NULL, NULL, 1, '', 'admin', '2023-05-20 04:01:28', 'admin', '2023-05-20 04:01:28', 0);
+INSERT INTO `mbs_user` VALUES (6946592389145600, 'z1s11', '', '', NULL, '$2a$10$/OUZIqJV2HjZfd3Dt3Agd.6pWH2p4xOVdD3jwzcCNsCqvPNBf6Eqq', '', '', '', NULL, NULL, 1, '', 'admin', '2023-05-20 04:03:17', 'admin', '2023-05-20 04:03:17', 0);
+INSERT INTO `mbs_user` VALUES (6948303019254784, '111', '', '', NULL, '$2a$10$T8pAifn4ve1kF0jSBG1mm.CxcK1lqpv/hF74Z4ToKPP.gnlPV5i3W', '', '', '', NULL, NULL, 1, '', 'admin', '2023-05-20 04:10:00', 'admin', '2023-05-20 04:10:00', 0);
+INSERT INTO `mbs_user` VALUES (6949309643826176, '11111', '', '', NULL, '$2a$10$rfd9oROKrJ57PEBPCCu5MOkop.eToalFVTwO3DHvYSm6mOrwEzf2a', '', '', '', NULL, NULL, 1, '', 'admin', '2023-05-20 04:14:05', 'admin', '2023-05-20 04:14:05', 0);
 
 -- ----------------------------
 -- Table structure for mbs_user_role
@@ -145,10 +317,16 @@ CREATE TABLE `mbs_user_role`  (
   INDEX `fk_role_id`(`role_id`) USING BTREE COMMENT '外键角色id索引',
   CONSTRAINT `fk_role_id` FOREIGN KEY (`role_id`) REFERENCES `mbs_role` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `mbs_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '系统管理-用户角色中间表' ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '系统管理服务-用户角色中间表' ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of mbs_user_role
 -- ----------------------------
+INSERT INTO `mbs_user_role` VALUES (6943878947087360, 6943878900950016, 1, 'admin', '2023-05-20 03:52:30', 'admin', '2023-05-20 03:52:30');
+INSERT INTO `mbs_user_role` VALUES (6946154264732672, 6946154164069376, 1, 'admin', '2023-05-20 04:01:32', 'admin', '2023-05-20 04:01:32');
+INSERT INTO `mbs_user_role` VALUES (6946592443671552, 6946592389145600, 1, 'admin', '2023-05-20 04:03:17', 'admin', '2023-05-20 04:03:17');
+INSERT INTO `mbs_user_role` VALUES (6948316680102912, 6948303019254784, 1, 'admin', '2023-05-20 04:10:05', 'admin', '2023-05-20 04:10:05');
+INSERT INTO `mbs_user_role` VALUES (6949309799015424, 6949309643826176, 1, 'admin', '2023-05-20 04:14:05', 'admin', '2023-05-20 04:14:05');
+INSERT INTO `mbs_user_role` VALUES (6950025351472128, 2434570888750079, 1, 'admin', '2023-05-20 04:16:55', 'admin', '2023-05-20 04:16:55');
 
 SET FOREIGN_KEY_CHECKS = 1;
