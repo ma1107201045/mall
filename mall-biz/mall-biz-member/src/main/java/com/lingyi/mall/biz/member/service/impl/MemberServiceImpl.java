3902c397bc0ec2eec785431d364790a6a1c9f6ff
maweiyan <1107201045@qq.com>
2023-06-02 16:19:01 +0800

    类路径更改

--- mall-biz/mall-biz-member/src/main/java/com/lingyi/mall/biz/member/service/impl/MemberServiceImpl.java
+++ mall-biz/mall-biz-member/src/main/java/com/lingyi/mall/biz/member/service/impl/MemberServiceImpl.java
@@ -10,7 +10,7 @@ import com.lingyi.mall.biz.member.repository.MemberRepository;
 import com.lingyi.mall.biz.member.service.MemberService;
 import com.lingyi.mall.common.base.exception.BizException;
 import com.lingyi.mall.common.base.param.BasePageParam;
-import com.lingyi.mall.common.util.ConverterUtil;
+import com.lingyi.mall.common.base.util.ConverterUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
