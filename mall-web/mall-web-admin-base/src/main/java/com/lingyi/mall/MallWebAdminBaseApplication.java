0be7534c118fd45d6e41a42d5499de740fef54b9
maweiyan <1107201045@qq.com>
2023-06-07 11:03:56 +0800

    测试用例修正2

--- mall-web/mall-web-admin-base/src/main/java/com/lingyi/mall/MallWebAdminBaseApplication.java
+++ mall-web/mall-web-admin-base/src/main/java/com/lingyi/mall/MallWebAdminBaseApplication.java
@@ -3,7 +3,6 @@ package com.lingyi.mall;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 /**
@@ -13,7 +12,6 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
  * @description
  */
 @EnableFeignClients
-@EnableJpaAuditing
 @EnableMethodSecurity
 @SpringBootApplication
 public class MallWebAdminBaseApplication {
