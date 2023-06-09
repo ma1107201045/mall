cb01cf2cf7e7ca5ef01275ec481403fcdbb4f573
maweiyan <1107201045@qq.com>
2023-06-09 19:58:20 +0800

    项目结构改变4

--- mall-common/mall-common-base/src/main/java/com/lingyi/mall/common/base/task/BaseAsyncTask.java
+++ mall-common/mall-common-base/src/main/java/com/lingyi/mall/common/base/task/BaseAsyncTask.java
@@ -7,8 +7,6 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 /**
- * TODO 待优化（由于maven存在循环依赖，暂且只能通过反射调用类）
- *
  * @Author: maweiyan
  * @Email 1107201045@qq.com
  * @DateTime: 2023/6/3 20:38
