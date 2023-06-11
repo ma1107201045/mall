faf82dd01ac829a3be5edb393ac10e214012738b
maweiyan <1107201045@qq.com>
2023-06-11 16:42:42 +0800

    验证码优化

--- mall-auth/mall-auth-admin/src/main/java/com/lingyi/mall/auth/admin/controller/AdminController.java
+++ mall-auth/mall-auth-admin/src/main/java/com/lingyi/mall/auth/admin/controller/AdminController.java
@@ -1,22 +1,15 @@
 package com.lingyi.mall.auth.admin.controller;
 
-import cn.hutool.captcha.ICaptcha;
 import com.lingyi.mall.auth.admin.properties.CaptchaProperties;
 import com.lingyi.mall.auth.admin.util.CaptchaUtil;
 import com.lingyi.mall.common.base.aspect.Log;
 import com.lingyi.mall.common.base.enums.OperationTypeEnum;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.io.IOException;
-import java.io.OutputStream;
 
 /**
  * @author maweiyan
@@ -35,14 +28,8 @@ public class AdminController {
     @Operation(summary = "获取验证码", description = "获取验证码")
     @GetMapping("/get-captcha")
     @Log(title = "获取验证码", operationType = OperationTypeEnum.READ, ignoreParam = true)
-    public void get(HttpSession session, HttpServletResponse response) throws IOException {
-        response.setContentType(MediaType.IMAGE_PNG_VALUE);
-        ICaptcha captcha = CaptchaUtil.get(captchaProperties);
-        session.setAttribute(captchaProperties.getSessionAttributeName(), captcha.getCode());
-        OutputStream os = response.getOutputStream();
-        captcha.write(os);
-        os.close();
-        os.flush();
+    public void getCaptcha() {
+        CaptchaUtil.write(captchaProperties);
     }
 
 }
--- mall-auth/mall-auth-admin/src/main/java/com/lingyi/mall/auth/admin/util/CaptchaUtil.java
+++ mall-auth/mall-auth-admin/src/main/java/com/lingyi/mall/auth/admin/util/CaptchaUtil.java
@@ -2,6 +2,18 @@ package com.lingyi.mall.auth.admin.util;
 
 import cn.hutool.captcha.ICaptcha;
 import com.lingyi.mall.auth.admin.properties.CaptchaProperties;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.lang.NonNull;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Objects;
 
 /**
  * @author maweiyan
@@ -9,6 +21,7 @@ import com.lingyi.mall.auth.admin.properties.CaptchaProperties;
  * @datetime 2023/5/20 8:55
  * @description
  */
+@Slf4j
 public class CaptchaUtil {
 
 
@@ -27,6 +40,33 @@ public class CaptchaUtil {
             case GIF ->
                     cn.hutool.captcha.CaptchaUtil.createGifCaptcha(properties.getWidth(), properties.getHeight(), properties.getCount());
         };
+
+    }
+
+
+    public static void write(CaptchaProperties captchaProperties) {
+        ServletRequestAttributes servletRequestAttributes = ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes()));
+        HttpSession session = servletRequestAttributes.getRequest().getSession();
+        HttpServletResponse response = Objects.requireNonNull(servletRequestAttributes.getResponse());
+        response.setContentType(MediaType.IMAGE_PNG_VALUE);
+        ICaptcha captcha = CaptchaUtil.get(captchaProperties);
+        session.setAttribute(captchaProperties.getSessionAttributeName(), captcha.getCode());
+        OutputStream os = null;
+        try {
+            os = response.getOutputStream();
+            captcha.write(os);
+        } catch (IOException e) {
+            log.error("验证码有误", e);
+        } finally {
+            if (Objects.nonNull(os)) {
+                try {
+                    os.close();
+                    os.flush();
+                } catch (IOException e) {
+                    log.error("关闭流失败", e);
+                }
+            }
+        }
 
     }
 
