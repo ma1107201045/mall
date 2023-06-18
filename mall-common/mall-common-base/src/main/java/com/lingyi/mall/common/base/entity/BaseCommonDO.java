810d400b480cde602abbccfee6706294dd92e570
maweiyan <1107201045@qq.com>
2023-06-18 20:17:46 +0800

    跨域配置设置到网关2

--- mall-common/mall-common-base/src/main/java/com/lingyi/mall/common/base/entity/BaseCommonDO.java
+++ mall-common/mall-common-base/src/main/java/com/lingyi/mall/common/base/entity/BaseCommonDO.java
@@ -34,7 +34,7 @@ public abstract class BaseCommonDO extends BaseIdDO {
 
     @CreatedDate
     @Column(name = "create_date_time", updatable = false, columnDefinition = "DATETIME(6) NOT NULL COMMENT '创建时间'")
-    @JsonFormat(pattern = "yyyy-MM-DD HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     protected LocalDateTime createDateTime;
 
     @LastModifiedBy
@@ -43,7 +43,7 @@ public abstract class BaseCommonDO extends BaseIdDO {
 
     @LastModifiedDate
     @Column(name = "last_modify_date_time", columnDefinition = "DATETIME(6) NOT NULL COMMENT '最后修改时间'")
-    @JsonFormat(pattern = "yyyy-MM-DD HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     protected LocalDateTime lastModifyDateTime;
 
 }
--- mall-web/mall-web-admin-base/src/main/java/com/lingyi/mall/web/admin/base/controller/BaseController.java
+++ mall-web/mall-web-admin-base/src/main/java/com/lingyi/mall/web/admin/base/controller/BaseController.java
@@ -3,6 +3,8 @@ package com.lingyi.mall.web.admin.base.controller;
 import com.lingyi.mall.api.system.dto.MenuResDTO;
 import com.lingyi.mall.api.system.dto.UserPartReqDTO;
 import com.lingyi.mall.biz.base.service.BaseService;
+import com.lingyi.mall.common.base.aspect.Log;
+import com.lingyi.mall.common.base.enums.OperationTypeEnum;
 import com.lingyi.mall.common.base.util.AuthenticatorUtil;
 import com.lingyi.mall.common.util.ServerResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -31,14 +33,16 @@ public class BaseController {
 
     @Operation(summary = "获取菜单", description = "获取菜单")
     @GetMapping("/menu")
+    @Log(title = "获取当前用户菜单", operationType = OperationTypeEnum.READ)
     public ServerResponse<List<MenuResDTO>> getMenu() {
         String userName = AuthenticatorUtil.getUserName();
         List<MenuResDTO> menus = baseService.readMenuTreeByUserName(userName);
         return ServerResponse.success(menus);
     }
 
-    @Operation(summary = "更新用户信息", description = "更新用户信息")
+    @Operation(summary = "更新用户", description = "更新用户")
     @PatchMapping("/user")
+    @Log(title = "更新当前用户信息", operationType = OperationTypeEnum.UPDATE)
     public ServerResponse<Void> updateUser(UserPartReqDTO userPartReqDTO) {
         Long userId = AuthenticatorUtil.getUserId();
         baseService.updateUserByUserId(userId, userPartReqDTO);
