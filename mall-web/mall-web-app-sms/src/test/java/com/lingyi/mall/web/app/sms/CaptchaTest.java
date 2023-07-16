2d8acb60edeaeed5f67e4da1f5848ca9197977d1
maweiyan <1107201045@qq.com>
2023-07-16 19:42:42 +0800

    分布式锁注解4

--- mall-web/mall-web-app-sms/src/test/java/com/lingyi/mall/web/app/sms/CaptchaTest.java
+++ mall-web/mall-web-app-sms/src/test/java/com/lingyi/mall/web/app/sms/CaptchaTest.java
@@ -2,6 +2,7 @@ package com.lingyi.mall.web.app.sms;
 
 import cn.hutool.core.thread.ThreadUtil;
 import cn.hutool.core.util.RandomUtil;
+import cn.hutool.core.util.StrUtil;
 import com.lingyi.mall.MallWebAppSmsApplicationTest;
 import com.lingyi.mall.api.sms.dto.CaptchaSendReqDTO;
 import com.lingyi.mall.api.sms.enums.BusinessTypeEnum;
@@ -48,21 +49,9 @@ public class CaptchaTest implements MallWebAppSmsApplicationTest {
 
     @Test
     public void testThreadCaptchaSend() throws IOException {
-        ExecutorService executorService = ThreadUtil.newFixedExecutor(100, "11", false);
+        ExecutorService executorService = ThreadUtil.newFixedExecutor(100, StrUtil.EMPTY, false);
         for (int i = 0; i < 100; i++) {
-            executorService.submit(() -> {
-                CaptchaSendReqDTO captchaSendReqDTO = new CaptchaSendReqDTO();
-                captchaSendReqDTO.setServiceType(ServiceTypeEnum.MALL_AUTH_APP.getCode());
-                captchaSendReqDTO.setBusinessType(BusinessTypeEnum.LOGIN.getCode());
-                captchaSendReqDTO.setPhoneNumber("15038233127");
-                captchaSendReqDTO.setCaptcha(RandomUtil.randomNumbers(6));
-                captchaSendReqDTO.setLength(6);
-                captchaSendReqDTO.setExpiryDate(30);
-                captchaSendReqDTO.setIntervalDate(1);
-                captchaSendReqDTO.setUpperLimit(10);
-                captchaSendReqDTO.setRemark(ServiceTypeEnum.UNKNOWN.getMessage() + BaseConstant.COLON_CHAR + BusinessTypeEnum.UNKNOWN.getMessage());
-                captchaService.send(captchaSendReqDTO);
-            });
+            executorService.submit(this::testCaptchaSend);
         }
         System.in.read();
     }
