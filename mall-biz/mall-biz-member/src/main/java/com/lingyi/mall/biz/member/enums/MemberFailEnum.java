7421df8e541c6240738091a9a91b9a283ce5e87f
maweiyan <1107201045@qq.com>
2023-07-17 17:51:08 +0800

    会员等级错误修改

--- mall-biz/mall-biz-member/src/main/java/com/lingyi/mall/biz/member/enums/MemberFailEnum.java
+++ mall-biz/mall-biz-member/src/main/java/com/lingyi/mall/biz/member/enums/MemberFailEnum.java
@@ -16,7 +16,7 @@ public enum MemberFailEnum implements BaseFailEnum {
 
     MEMBER_NULL_ERROR(8001, "会员不能为空"),
 
-    MEMBER_LEVEL_NULL_ERROR(8002, "会员等级不饿能为空"),
+    MEMBER_LEVEL_NULL_ERROR(8002, "会员等级不能为空"),
 
     MEMBER_LOGIN_LOG_NULL_ERROR(8002, "会员登录日志不能为空");
 
