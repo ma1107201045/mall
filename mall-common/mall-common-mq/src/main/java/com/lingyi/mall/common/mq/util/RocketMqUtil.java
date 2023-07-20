bda1a7d867dbb1ece7a782d7968f5267e613a640
maweiyan <1107201045@qq.com>
2023-07-20 14:50:23 +0800

    rocketmq

---
+++ mall-common/mall-common-mq/src/main/java/com/lingyi/mall/common/mq/util/RocketMqUtil.java
@@ -0,0 +1,17 @@
+package com.lingyi.mall.common.mq.util;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author maweiyan
+ * @email 1107201045@qq.com
+ * @datetime 2023/7/20 9:35
+ * @description
+ */
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class RocketMqUtil {
+}
---
+++ mall-web/mall-web-app-sms/src/test/java/com/lingyi/mall/web/app/sms/RocketMqTest.java
@@ -0,0 +1,72 @@
+package com.lingyi.mall.web.app.sms;
+
+import com.lingyi.mall.MallWebAppSmsApplicationTest;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.client.producer.SendCallback;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+
+/**
+ * @author maweiyan
+ * @email 1107201045@qq.com
+ * @datetime 2023/7/20 10:08
+ * @description
+ */
+@Slf4j
+@SpringBootTest
+public class RocketMqTest implements MallWebAppSmsApplicationTest {
+
+    @Autowired
+    private RocketMQTemplate rocketMQTemplate;
+
+    @Test
+    public void testSendMessage() {
+        rocketMQTemplate.send("Topic01", new Message<>() {
+            @NotNull
+            @Override
+            public Object getPayload() {
+                return "Hello World !!";
+            }
+
+            @NotNull
+            @Override
+            public MessageHeaders getHeaders() {
+                return new MessageHeaders(null);
+            }
+        });
+
+    }
+
+    @Test
+    public void testAsyncSendMessage() {
+        rocketMQTemplate.asyncSend("Topic02", new Message<>() {
+            @NotNull
+            @Override
+            public Object getPayload() {
+                return "Hello World !!";
+            }
+
+            @NotNull
+            @Override
+            public MessageHeaders getHeaders() {
+                return new MessageHeaders(null);
+            }
+        }, new SendCallback() {
+            @Override
+            public void onSuccess(SendResult sendResult) {
+                log.info(sendResult.toString());
+            }
+
+            @Override
+            public void onException(Throwable throwable) {
+                log.info("sendError", throwable);
+            }
+        });
+    }
+}
