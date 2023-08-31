c5cf0b6889a31035e5088311880d0d535f6d2980
maweiyan <1107201045@qq.com>
2023-08-31 09:28:13 +0800

    product

---
+++ mall-biz/mall-biz-product/src/main/java/com/lingyi/mall/biz/product/mapper/CategoryAttributeMapper.java
@@ -0,0 +1,13 @@
+package com.lingyi.mall.biz.product.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @Author: maweiyan
+ * @Email: 1107201045@qq.com
+ * @DateTime: 2023/8/31 9:25
+ * @Description:
+ */
+@Mapper
+public interface CategoryAttributeMapper {
+}
---
+++ mall-biz/mall-biz-product/src/main/java/com/lingyi/mall/biz/product/repository/CategoryAttributeRepository.java
@@ -0,0 +1,16 @@
+package com.lingyi.mall.biz.product.repository;
+
+import com.lingyi.mall.biz.product.entity.CategoryAttributeDO;
+import com.lingyi.mall.biz.product.entity.CategoryDO;
+import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @Author: maweiyan
+ * @Email: 1107201045@qq.com
+ * @DateTime: 2023/8/31 9:26
+ * @Description:
+ */
+@Repository
+public interface CategoryAttributeRepository extends JpaRepositoryImplementation<CategoryAttributeDO, Long> {
+}
