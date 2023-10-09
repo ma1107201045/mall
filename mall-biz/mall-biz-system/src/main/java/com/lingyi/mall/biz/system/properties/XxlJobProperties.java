941d7db20723902130d17ee5b45e162c34ee38a9
maweiyan <1107201045@qq.com>
2023-10-09 14:45:26 +0800

    xxl job add2

--- mall-biz/mall-biz-system/src/main/java/com/lingyi/mall/biz/system/properties/XxlJobProperties.java
+++ mall-biz/mall-biz-system/src/main/java/com/lingyi/mall/biz/system/properties/XxlJobProperties.java
@@ -15,19 +15,38 @@ import org.springframework.context.annotation.Configuration;
 @ConfigurationProperties(prefix = "xxl-job")
 public class XxlJobProperties {
 
+    /**
+     * admin地址### xxl-job admin address list, such as "http://address" or "http://address01,http://address02"
+     */
     private String adminAddresses;
 
+    /**
+     * 访问token
+     */
     private String accessToken;
 
+    /**
+     * app名称
+     */
     private String appName;
-
+    /**
+     * ### xxl-job executor registry-address: default use address to registry , otherwise use ip:port if address is null
+     */
     private String address;
-
+    /**
+     * ### xxl-job executor server-info
+     */
     private String ip;
+
 
     private int port;
 
+    /**
+     * 日志路径
+     */
     private String logPath;
-
+    /**
+     * 日志路径
+     */
     private int logRetentionDays;
 }
