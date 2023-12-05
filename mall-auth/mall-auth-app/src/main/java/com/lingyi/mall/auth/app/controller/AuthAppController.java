b72e99b8c69aa697f11130a2a30f93d80f1bddf4
maweiyan <1107201045@qq.com>
2023-12-05 14:30:32 +0800

    登录增加短信+邮箱登录3

--- mall-auth/mall-auth-app/src/main/java/com/lingyi/mall/auth/app/controller/AuthAppController.java
+++ mall-auth/mall-auth-app/src/main/java/com/lingyi/mall/auth/app/controller/AuthAppController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.*;
  * @description
  */
 @Tag(name = "认证", description = "认证")
-@Validated
 @RestController
 @RequestMapping("/auth/app")
 @RequiredArgsConstructor
