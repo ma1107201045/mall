33318668a50ed0eb05101157e86632be396e41f9
maweiyan <1107201045@qq.com>
2023-12-27 14:44:12 +0800

    swagger 优化2

---
+++ mall-web/mall-web-admin-system/src/main/java/com/lingyi/mall/web/admin/system/config/SwaggerConfig.java
@@ -0,0 +1,10 @@
+package com.lingyi.mall.web.admin.system.config;
+
+/**
+ * @Author: maweiyan
+ * @Email: 1107201045@qq.com
+ * @DateTime: 2023/12/18 10:14
+ * @Description:
+ */
+public class SwaggerConfig {
+}
