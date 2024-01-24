0c40b392255e652fac729cb8535fccb07a617fa7
maweiyan <1107201045@qq.com>
2024-01-24 16:56:53 +0800

    基础业务4

--- mall-api/mall-api-system/src/main/java/com/lingyi/mall/api/system/feign/LogFeign.java
+++ mall-api/mall-api-system/src/main/java/com/lingyi/mall/api/system/feign/LogFeign.java
@@ -5,6 +5,8 @@ import com.lingyi.mall.api.system.fallbackfactory.LogFeignFallbackFactory;
 import com.lingyi.mall.common.web.util.ServerResponse;
 import feign.Request;
 import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
  * @Description:
  */
 @Hidden
+@Tag(name = "系统日志", description = "系统日志")
 @FeignClient(value = "mall-web-admin-system", fallbackFactory = LogFeignFallbackFactory.class)
 public interface LogFeign {
 
@@ -28,6 +31,7 @@ public interface LogFeign {
      * @param options    options
      * @return ServerResponse<Void>
      */
+    @Operation(description = "保存")
     @PostMapping(URL_PREFIX)
     ServerResponse<Void> save(@RequestBody LogRequest logRequest, Request.Options options);
 }
