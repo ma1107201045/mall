aa7e29d8378169f70f26c762da4bc77717872a7c
maweiyan <1107201045@qq.com>
2024-01-24 10:48:18 +0800

    基础业务优化3

--- mall-security/mall-security-app/src/main/java/com/lingyi/mall/security/app/config/SaTokenConfigure.java
+++ mall-security/mall-security-app/src/main/java/com/lingyi/mall/security/app/config/SaTokenConfigure.java
@@ -1,14 +1,20 @@
 package com.lingyi.mall.security.app.config;
 
 import cn.dev33.satoken.config.SaTokenConfig;
+import cn.dev33.satoken.jwt.StpLogicJwtForMixin;
+import cn.dev33.satoken.jwt.StpLogicJwtForSimple;
+import cn.dev33.satoken.jwt.StpLogicJwtForStateless;
 import cn.dev33.satoken.stp.StpInterface;
+import cn.dev33.satoken.stp.StpLogic;
 import com.lingyi.mall.api.member.consumer.MemberFeignConsumer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @Author: maweiyan
@@ -19,33 +25,26 @@ import java.util.List;
 @Configuration(proxyBeanMethods = false)
 public class SaTokenConfigure {
 
-    private static final String PERMISSION_SEPARATOR = ",";
+    // Sa-Token 整合 jwt (Simple 简单模式)
+    // @Bean
+    public StpLogic getStpLogicJwtForSimple() {
+        return new StpLogicJwtForSimple();
+    }
 
-    // Sa-Token 参数配置，参考文档：https://sa-token.cc
-    // 此配置会覆盖 application.yml 中的配置
+    // Sa-Token 整合 jwt (Mixin 混入模式)
+    //  @Bean
+    public StpLogic getStpLogicJwtForMixin() {
+        return new StpLogicJwtForMixin();
+    }
+
+    // Sa-Token 整合 jwt (Stateless 无状态模式)
     @Bean
-    @Primary
-    public SaTokenConfig saTokenConfig() {
-        SaTokenConfig config = new SaTokenConfig();
-        // token 名称（同时也是 cookie 名称）
-        config.setTokenName("satoken");
-        // token 有效期（单位：秒），默认30天，-1代表永不过期
-        config.setTimeout(30 * 24 * 60 * 60);
-        // token 最低活跃频率（单位：秒），如果 token 超过此时间没有访问系统就会被冻结，默认-1 代表不限制，永不冻结
-        config.setActiveTimeout(-1);
-        // 是否允许同一账号多地同时登录（为 true 时允许一起登录，为 false 时新登录挤掉旧登录）
-        config.setIsConcurrent(true);
-        // 在多人登录同一账号时，是否共用一个 token （为 true 时所有登录共用一个 token，为 false 时每次登录新建一个 token）
-        config.setIsShare(true);
-        // token 风格
-        config.setTokenStyle("uuid");
-        // 是否输出操作日志
-        config.setIsLog(false);
-        return config;
+    public StpLogic getStpLogicJwtForStateless() {
+        return new StpLogicJwtForStateless();
     }
 
     @Bean
-    public StpInterface stpInterface(MemberFeignConsumer memberFeignConsumer) {
+    public StpInterface stpInterface() {
         return new StpInterface() {
             @Override
             public List<String> getPermissionList(Object loginId, String loginType) {
